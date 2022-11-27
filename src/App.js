c04fd66a83a7e210e7ade19d0ca64b04c8182c31
VaibhawKr744 <honeykr744@gmail.com>
2022-11-27 16:48:14 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Landing from "./pages/Landing";
+
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import {Dashboard, Register, Error, Landing} from './pages'
 function App() {
   return (
     
       <BrowserRouter>
+      
         <Routes>
-          <Route path='/' element={<div>Dashboard</div>} />
-          <Route path='/register' element={<div>Register</div>} />
+          <Route path='/' element={<Dashboard/>} />
+          <Route path='/register' element={<Register/>} />
           <Route path='/landing' element={<Landing/>} />
-          <Route path='*' element={<h1>Error</h1>} />
-          <Landing />
+          <Route path='*' element={<Error/>} />
+         
         </Routes>
       </BrowserRouter>
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,7 @@
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import  {Logo}  from "../components"
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const Landing = () => {
   return (
     <Wrapper>
@@ -20,9 +20,9 @@ const Landing = () => {
             quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
             narwhal.
           </p>
-          {/* <Link to='/register' className='btn btn-hero'>
+          <Link to='/register' className='btn btn-hero'>
             Login/Register
-          </Link> */}
+          </Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
       </div>
